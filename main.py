d6c7ec554512fefd7ee88d6e004ffd25127f7017
Andrew Praskala <84627619+aprask@users.noreply.github.com>
2024-01-15 20:07:31 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+given_sentence = input("Give me a sentence: ")
+print("The sentence you gave was: " + given_sentence)
+original_word = input("What word would you like to replace? ")
+new_word = input("What is the new word? ")
+print(given_sentence.replace(original_word,new_word))
